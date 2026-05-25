51b6ad2c13929537a163d890f78cd6dfba25c47b
Antonio Becic <antoniobecic@Mac-2.home>
2026-05-25 20:42:19 +0200

    fix: lock demo pages to English regardless of user locale

--- app/demo/layout.tsx
+++ app/demo/layout.tsx
@@ -1,10 +1,13 @@
+import { AppUiProvider } from "@/components/AppUiProvider";
+import { getAppStrings } from "@/lib/app-ui";
 import { DemoThemeLock } from "./_components/DemoThemeLock";
 
 export default function DemoLayout({ children }: { children: React.ReactNode }) {
+  const dict = getAppStrings("en");
   return (
-    <>
+    <AppUiProvider value={{ locale: "en", ...dict }}>
       <DemoThemeLock />
       {children}
-    </>
+    </AppUiProvider>
   );
 }
